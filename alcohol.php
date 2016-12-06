1f8ca2f586213bbea5c24953b43aa31b6ebfcf87
jsgonz <j.gonzalez@ufl.edu>
2016-12-06 16:45:02 -0500

    css
    some css stuff

--- alcohol.php
+++ alcohol.php
@@ -1,221 +1,221 @@
-#!/usr/local/bin/php
 <!DOCTYPE html>
 <html>
 	<head>
+		<link rel="stylesheet" type="text/css" href="ourstyle.css">
   	<title>Database Project </title>
 	</head>
 	<body>
-  	<h1>2014 NSDUH Statistics on Alcohol</h1>
-    <h3>
-      What would you like to know about Alcohol users?
-    </h3>
-    	<p>
-    		<form method="post">
-    			<select id="select" name="ONE">
-    				<option value="nothing">
-    					Choose...
-    				</option>
-    				<option value="amount">
-    					Amount
-    				</option>
-    				<option value="percentage">
-    					Percentage
-    				</option>
-    			</select>
-    			of Alcohol users?
-    			<input type="submit" name="submit">
-    		</form>
-    		<!--(Amount) of people who use (substance)?
-    		(Percentage) of people who use (substance)?-->
-    	</p>
-    	<p>
-    		<form method="post">
-    			<select id="select" name="ONE">
-    				<option value="nothing">
-    					Choose...
-    				</option>
-    				<option value="amount">
-    					Amount
-    				</option>
-    				<option value="percentage">
-    					Percentage
-    				</option>
-    			</select>
-    			of Alcohol users who (are)
-    			<select id="select" name="TWO">
-    				<option value="nothing">
-    					Choose...
-    				</option>
-    				<option value="makemore">
-    					Make more than
-    				</option>
-    				<option value="makeless">
-    					Make less than
-    				</option>
-    				<option value="unemployed">
-    					Unemployed
-    				</option>
-    				<option value="employed">
-    					Employed
-    				</option>
-    				<option value="nhw">
-    					Non-Hispanic White
-    				</option>
-    				<option value="hispanic">
-    					Hispanic
-    				</option>
-    				<option value="nhb">
-    					Non-Hispanic Black/African-American
-    				</option>
-    				<option value="nhpi">
-    					Non-Hispanic Pacific Islander
-    				</option>
-    				<option value="nhm">
-    					Non-Hispanic Multiracial
-    				</option>
-    			</select>
+		<div id="content">
+	  	<h1>2014 NSDUH Statistics on Alcohol</h1>
+	    <h3>
+	      What would you like to know about Alcohol users?
+	    </h3>
+	    	<p>
+	    		<form method="post">
+	    			<select id="select" name="ONE">
+	    				<option value="nothing">
+	    					Choose...
+	    				</option>
+	    				<option value="amount">
+	    					Amount
+	    				</option>
+	    				<option value="percentage">
+	    					Percentage
+	    				</option>
+	    			</select>
+	    			of Alcohol users?
+	    			<input type="submit" name="submit">
+	    		</form>
+	    		<!--(Amount) of people who use (substance)?
+	    		(Percentage) of people who use (substance)?-->
+	    	</p>
+	    	<p>
+	    		<form method="post">
+	    			<select id="select" name="ONE">
+	    				<option value="nothing">
+	    					Choose...
+	    				</option>
+	    				<option value="amount">
+	    					Amount
+	    				</option>
+	    				<option value="percentage">
+	    					Percentage
+	    				</option>
+	    			</select>
+	    			of Alcohol users who (are)
+	    			<select id="select" name="TWO">
+	    				<option value="nothing">
+	    					Choose...
+	    				</option>
+	    				<option value="makemore">
+	    					Make more than
+	    				</option>
+	    				<option value="makeless">
+	    					Make less than
+	    				</option>
+	    				<option value="unemployed">
+	    					Unemployed
+	    				</option>
+	    				<option value="employed">
+	    					Employed
+	    				</option>
+	    				<option value="nhw">
+	    					Non-Hispanic White
+	    				</option>
+	    				<option value="hispanic">
+	    					Hispanic
+	    				</option>
+	    				<option value="nhb">
+	    					Non-Hispanic Black/African-American
+	    				</option>
+	    				<option value="nhpi">
+	    					Non-Hispanic Pacific Islander
+	    				</option>
+	    				<option value="nhm">
+	    					Non-Hispanic Multiracial
+	    				</option>
+	    			</select>
+						?
+	    			<input type="submit" name="submit">
+	    		</form>
+	    		<!--(Amount) of people who (condition)?
+	    		(Percentage) of people who (condition)?-->
+	    	</p>
+			<p>
+				<form method="post">
+					<select id="select" name="STATS1">
+						<option value="nothing">
+							Choose...
+						</option>
+						<option value="average">
+							Average
+						</option>
+						<option value="mode">
+							Mode
+						</option>
+						<option value="median">
+							Median
+						</option>
+						<option value="stddev">
+							Standard Deviation
+						</option>
+					</select>
+					<select id="select" name="STATS2">
+						<option value="nothing">
+							Choose...
+						</option>
+						<option value="age">
+							Age
+						</option>
+						<option value="income">
+							Income
+						</option>
+						<option value="firstuse">
+							Age of first use
+						</option>
+					</select>
+					of Alcohol users?
+					<input type="submit" name="submit">
+				</form>
+				<!--(Average) (value) of people who use (substance)?
+				(Mode) (value) of people who use (substance)?
+				(Median) (value) of people who use (substance)?
+				(Standard deviation) (value) of people who use (substance)?-->
+			</p>
+			<p>
+				<form method="post">
+					<select id="select" name="STATS1">
+						<option value="nothing">
+							Choose...
+						</option>
+						<option value="average">
+							Average
+						</option>
+						<option value="mode">
+							Mode
+						</option>
+						<option value="median">
+							Median
+						</option>
+						<option value="stddev">
+							Standard Deviation
+						</option>
+					</select>
+					<select id="select" name="STATS2">
+						<option value="nothing">
+							Choose...
+						</option>
+						<option value="age">
+							Age
+						</option>
+						<option value="income">
+							Income
+						</option>
+						<option value="firstuse">
+							Age of first use
+						</option>
+					</select>
+					of Alcohol users who work in the field of
+					<select id="select" name="STATS3">
+						<option value="nothing">
+							Choose...
+						</option>
+						<option value="job1">
+							Executive/Administrative/Managerial/Financial
+						</option>
+	    			<option value="job2">
+	    				Professional (not Education/Entertainment/Media)
+	    			</option>
+	    			<option value="job3">
+	    				Education and Related Occupations
+	  				</option>
+	    			<option value="job4">
+	    				Entertainers, Sports, Media, and Communications
+	    			</option>
+	    			<option value="job5">
+	    				Technicians and Related Support Occupations
+	    			</option>
+	    			<option value="job6">
+	    				Sales Occupations
+	    			</option>
+	    			<option value="job7">
+	    				Office and Administrative Support Workers
+	    			</option>
+	    			<option value="job8">
+	    				Protective Service Occupations
+	    			</option>
+	    			<option value="job9">
+	    				Service Occupations, Except Protective
+	    			</option>
+	    			<option value="job10">
+	    				Farming, Fishing, and Forestry Occupations
+	    			</option>
+	    			<option value="job11">
+	    				Installation, Maintenance and Repair Workers
+	    			</option>
+	    			<option value="job12">
+	    				Construction Trades and Extraction Workers
+	    			</option>
+	    			<option value="job13">
+	    				Production, Machinery Setters/Operators/Tenders
+	    			</option>
+	  				<option value="job14">
+	    				Transportation and Material Moving Workers
+	  				</option>
+						<option value="job15">
+	    				Unknown or unemployed
+	    			</option>
+					</select>
 					?
-    			<input type="submit" name="submit">
-    		</form>
-    		<!--(Amount) of people who (condition)?
-    		(Percentage) of people who (condition)?-->
-    	</p>
-		<p>
-			<form method="post">
-				<select id="select" name="STATS1">
-					<option value="nothing">
-						Choose...
-					</option>
-					<option value="average">
-						Average
-					</option>
-					<option value="mode">
-						Mode
-					</option>
-					<option value="median">
-						Median
-					</option>
-					<option value="stddev">
-						Standard Deviation
-					</option>
-				</select>
-				<select id="select" name="STATS2">
-					<option value="nothing">
-						Choose...
-					</option>
-					<option value="age">
-						Age
-					</option>
-					<option value="income">
-						Income
-					</option>
-					<option value="firstuse">
-						Age of first use
-					</option>
-				</select>
-				of Alcohol users?
-				<input type="submit" name="submit">
-			</form>
-			<!--(Average) (value) of people who use (substance)?
-			(Mode) (value) of people who use (substance)?
-			(Median) (value) of people who use (substance)?
-			(Standard deviation) (value) of people who use (substance)?-->
-		</p>
-		<p>
-			<form method="post">
-				<select id="select" name="STATS1">
-					<option value="nothing">
-						Choose...
-					</option>
-					<option value="average">
-						Average
-					</option>
-					<option value="mode">
-						Mode
-					</option>
-					<option value="median">
-						Median
-					</option>
-					<option value="stddev">
-						Standard Deviation
-					</option>
-				</select>
-				<select id="select" name="STATS2">
-					<option value="nothing">
-						Choose...
-					</option>
-					<option value="age">
-						Age
-					</option>
-					<option value="income">
-						Income
-					</option>
-					<option value="firstuse">
-						Age of first use
-					</option>
-				</select>
-				of Alcohol users who work in the field of
-				<select id="select" name="STATS3">
-					<option value="nothing">
-						Choose...
-					</option>
-					<option value="job1">
-						Executive/Administrative/Managerial/Financial
-					</option>
-    			<option value="job2">
-    				Professional (not Education/Entertainment/Media)
-    			</option>
-    			<option value="job3">
-    				Education and Related Occupations
-  				</option>
-    			<option value="job4">
-    				Entertainers, Sports, Media, and Communications
-    			</option>
-    			<option value="job5">
-    				Technicians and Related Support Occupations
-    			</option>
-    			<option value="job6">
-    				Sales Occupations
-    			</option>
-    			<option value="job7">
-    				Office and Administrative Support Workers
-    			</option>
-    			<option value="job8">
-    				Protective Service Occupations
-    			</option>
-    			<option value="job9">
-    				Service Occupations, Except Protective
-    			</option>
-    			<option value="job10">
-    				Farming, Fishing, and Forestry Occupations
-    			</option>
-    			<option value="job11">
-    				Installation, Maintenance and Repair Workers
-    			</option>
-    			<option value="job12">
-    				Construction Trades and Extraction Workers
-    			</option>
-    			<option value="job13">
-    				Production, Machinery Setters/Operators/Tenders
-    			</option>
-  				<option value="job14">
-    				Transportation and Material Moving Workers
-  				</option>
-					<option value="job15">
-    				Unknown or unemployed
-    			</option>
-				</select>
-				?
-				<input type="submit" name="submit">
-			</form>
-			<!--(Average) (value) of people who use (substance)?
-			(Mode) (value) of people who use (substance)?
-			(Median) (value) of people who use (substance)?
-			(Standard deviation) (value) of people who use (substance)?-->
-		</p>
-
+					<input type="submit" name="submit">
+				</form>
+				<!--(Average) (value) of people who use (substance)?
+				(Mode) (value) of people who use (substance)?
+				(Median) (value) of people who use (substance)?
+				(Standard deviation) (value) of people who use (substance)?-->
+			</p>
     	<?php
-				$connection = oci_connect($username = 'jnovick',
+			/*	$connection = oci_connect($username = 'jnovick',
 																	$password = 'password',
 																	$connection_string = '//oracle.cise.ufl.edu/orcl');
 
@@ -234,6 +234,7 @@
 						$s = "SELECT COUNT(*)
 									FROM consumes
 									WHERE drug = 'Alcohol'";
+						$line = 'The amount of Alcohol users is ';
 					}
 					else if($second == 'makemore')
 					{
@@ -1280,6 +1281,8 @@
 			  $statement = oci_parse($connection, $s);
 			  oci_execute($statement);
 
+				echo $line;
+
 			  while (($row = oci_fetch_array($statement, OCI_BOTH)) != false)
 			  {
 					echo $row[0]."<br>";
@@ -1288,7 +1291,8 @@
 
 			  oci_free_statement($statement);
 
-			  oci_close($connection);
+			  oci_close($connection);*/
     	?>
+		</div>
 	</body>
 </html>
